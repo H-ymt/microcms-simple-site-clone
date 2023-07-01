b217ab13ed01c8ac6dd7f3176f75fe1c14ba9501
H-ymt <y.handai1272@gmail.com>
2023-07-01 13:55:12 +0900

    add newsページの追加

--- app/news/layout.tsx
+++ app/news/layout.tsx
@@ -3,3 +3,15 @@ import Hero from "@/components/hero";
 export const metadata = {
   title: "News | Corporate Site",
 };
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function RootLayout({ children }: Props) {
+  return (
+    <>
+      <Hero title="News" sub="ニュース" />
+    </>
+  );
+}
